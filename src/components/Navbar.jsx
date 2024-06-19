d3eed240580e9bc15ca6c0d9a2fe9ee1ed2ba476
Andre Ferreira <andre.f.v.ferreira@gmail.com>
2024-06-19 19:52:49 +0100

    Adds user to nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,14 @@
 import { Link } from "react-router-dom";
 
-function Navbar() {
+function Navbar({ user }) {
   return (
     <nav>
       <Link to={"/"}>
         <h1>IronMerch</h1>
       </Link>
+      {user ? <p>Welcome, {user.name}</p> : <Link to={"/signup"}>Signup</Link>}
       <div className="nav-links">
         <Link to={"/products"}> Products</Link>
-        <Link to={"/signup"}> Signup</Link>
       </div>
     </nav>
   );
